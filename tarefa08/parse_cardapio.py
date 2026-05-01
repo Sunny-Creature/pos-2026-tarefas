968ce79c01910bae67828509ce0783c18c6938ba
Sunny-Creature <soniaisissoninha119@gmail.com>
2026-04-30 23:23:14 -0300

    tarefa08 incompleta

--- tarefa08/parse_cardapio.py
+++ tarefa08/parse_cardapio.py
@@ -10,5 +10,24 @@ for prato in pratos:
     id = prato.getAttribute('id')
     elemento_nome = prato.getElementsByTagName('nome')[0]
     nome = elemento_nome.firstChild.nodeValue
-    elemento_descricao = prato.getElementsByTagName('descricao')[0]
-    descricao = elemento_descricao.firstChild.nodeValue
+    print(f'{id}- {nome}')
+    
+    opcao_prato = input("Digite o ID de um prato:")
+
+    if opcao_prato == prato.getAttribute('id'):
+        elemento_descricao = prato.getElementsByTagName('descricao')[0]
+        descricao = elemento_descricao.firstChild.nodeValue
+        ingrediente = prato.getElementsByTagName('ingrediente')
+        elemento_preco = prato.getElementsByTagName('preco')[0]
+        preco = elemento_preco.firstChild.nodeValue
+        elemento_calorias = prato.getElementsByTagName('calorias')[0]
+        calorias = elemento_calorias.firstChild.nodeValue
+
+    print('-'*10)
+    print("Nome:", nome)
+    print("Descrição:", descricao)
+    print("Ingredientes:")
+    for ingrediente in ingrediente:
+        print(ingrediente.firstChild.nodeValue)
+    print("Preço:", f'R${preco}')
+    print("Calorias:", calorias)
